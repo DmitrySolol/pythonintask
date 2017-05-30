f65126fd79861475c1f0a54a09ec09068d85d471
DmitrySolol <solo888000@gmail.com>
2017-05-30 23:16:38 +0300

    Add files via upload

---
+++ pin/2016/Naidishkin D.S/5.11.py
@@ -0,0 +1,12 @@
+﻿# Задача 5. Вариант 11.
+# Напишите программу, которая бы при запуске случайным образом отображала
+# имя одного из девяти оленей Санта Клауса.
+# Naidishkin D.S. 
+# 16.03.2017
+
+print ("Данная программа случайным образом отображает имя одного из девяти оленей Санта Клауса")
+import random
+import string
+deer = ["Dasher","Dancer","Prancer","Vixen","Comet","Cupid","Donder","Blitzen","Rudolf"]
+print("\nОдного из девяти оленей Санта Клауса звали ", random.choice(deer))
+input("\n\n\n\n\n\nPress Enter to exit from program...")
